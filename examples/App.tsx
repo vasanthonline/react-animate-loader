ae9add5f22fa9bd8e8733f1ca85fa99514fb9b7a
vasanthonline <vasanthgonline@gmail.com>
2020-10-09 12:45:51 +0530

    Add additional example for product tile

--- examples/App.tsx
+++ examples/App.tsx
@@ -6,22 +6,31 @@ type formData = {
   label?:  string,
   type?: string
 }
+type tileData = {
+  tileImage?: string,
+  title?: string,
+  subtitle?: string,
+  description?: string
+}
 type AppData = {
   block: string,
-  form : formData
+  form : formData,
+  tile : tileData
 }
 
 function App() {
   const defaultData: AppData = {
     'block': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
-    'form': {'label': 'Email address', 'type': 'email'}
+    'form': {'label': 'Email address', 'type': 'email'},
+    'tile': {'tileImage': 'https://dummyimage.com/250x200/eeeeee/fff&text=Sample+product+image', 'title': 'Nike Airmax T-shirt', 'subtitle': 'Highly breathable fabric with no sweat on skin', 'description': 'This product is made from at least 50% recycled polyester fabric.'},
   }
   const [ data, setData ] = useState(defaultData)
 
   function fetchData(){
     setData({
       'block': '',
-      'form': {}
+      'form': {},
+      'tile': {}
     })
     setTimeout(() => {
       setData(defaultData)
@@ -44,6 +53,21 @@ function App() {
                   </div>
                 </form>)
     return (data.form?.label ? el : <ReactLoader style={{minHeight: '10rem', backgroundColor: '#9fbdd2'}}><form className="form__group"></form></ReactLoader>)
+  }
+
+  function getTileElement(){
+    const el = (<>
+                  <div className="block__tile">
+                    {data.tile?.tileImage ? 
+                      <img src={data.tile?.tileImage} /> :
+                      ''
+                    }
+                  </div>
+                  <h3 className="block__tile__header">{data.tile?.title}</h3>
+                  <h4 className="block__tile__subheader">{data.tile?.subtitle}</h4>
+                  <p className="block__tile__text">{data.tile?.description}</p>
+                </>)
+    return (data.tile?.title ? el : <ReactLoader>{el.props.children}</ReactLoader>)
   }
 
   return (<>
@@ -70,6 +94,12 @@ function App() {
         <h2>Example 2 - Form element loader</h2>
         <span>(With different background color and customized height)</span>
         {getFormElement()}
+      </div>
+      <div className="block__row ">
+        <h2>Example 3 - Tile with Multiple elements loader</h2>
+        <div className="block__tile__border">
+          {getTileElement()}
+        </div>
       </div>
     </div>
   </>);
