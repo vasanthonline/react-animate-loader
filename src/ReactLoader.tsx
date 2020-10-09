be54cdf0bf434a9ceee7848bda1deb8d197f1355
vasanthonline <vasanthgonline@gmail.com>
2020-10-09 11:07:34 +0530

    Initial project setup with react and typescript

---
+++ src/ReactLoader.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './ReactLoader.css'
+
+type ReactLoaderProps = {
+
+}
+export default function ReactLoader(props:ReactLoaderProps) {
+
+  return (<div>Hello World!</div>)
+}
